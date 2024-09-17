a64a5fc33990a0d846b48b023e0a3ae53b0faf6a
kamthorn <kamthorn.sa@ksu.ac.th>
2024-09-17 15:50:50 +0700

    regis

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,15 +1,46 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useAuth } from "../AuthProvider";
+import { useState } from "react";
+import axios from "axios";
 
 function Login() {
+    const { login } = useAuth();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post('http://localhost:4000/login', {
+        email: email,
+        password: password
+      }, {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+      login(res.data.token);
+      alert('Login Successful!');
+      navigate('/');
+    } catch (err) {
+      setError(err.response ? err.response.data.message : 'Login failed');
+      console.error(err.response ? err.response.data : err); // Log the correct data
+    }
+  };
     return ( <>
      <div className="flex items-center justify-center min-h-screen bg-gray-100">
         <div className="p-8 bg-white mt-6 rounded-lg shadow-md">
-          <form className="p-3 w-96">
+            { error && <p className="text-red-500">{error}</p> }
+          <form onSubmit={handleSubmit} className="p-3 w-96">
             <h2 className="text-2xl font-bold mb-4">Login</h2>
              <div className="mb-4">
               <label className="block text-gray-700">Email</label>
               <input
                 type="email"
+                value={email}
+                onChange={e => setEmail(e.target.value)}
                 className="w-full p-2 border border-gray-300 rounded mt-2"
                 required
               />
@@ -18,6 +49,8 @@ function Login() {
               <label className="block text-gray-700">Password</label>
               <input
                 type="password"
+                value={password}
+                onChange={e => setPassword(e.target.value)}
                 className="w-full p-2 border border-gray-300 rounded mt-2"
                 required
               />
